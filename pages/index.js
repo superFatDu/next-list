9ae1dbb270916e3e7be39a4f2a067c2fa6cefb89
cheng5 <superduchengwu@163.com>
2021-07-22 11:15:56 +0800

    fix: update page name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Navbar from '../comps/NavBar'
+import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import Link from 'next/link'
 
